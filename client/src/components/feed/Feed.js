6a9da6d37b8523304cb8ff5b260865ac6140fbdf
James LaFarr <lafarrjam99@gmail.com>
2022-03-29 18:46:34 -0400

    Added comment functionality

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,15 +1,25 @@
 import Navigation from '../navigation/Navigation';
-import styles from './Feed.module.css';
-import './Feed.css';
-import { useState, useEffect, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
 import Dots from '../ui/dots-dropdown/Dots';
 import AuthContext from '../../context/AuthContext';
+import Modal from '../ui/modal/Modal';
+
+import styles from './Feed.module.css';
+import './Feed.css';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+
 
 const Feed = (props) => {
     const [posts, setPosts] = useState([]);
     const [likes, setLikes] = useState([]);
     const [num, setNum] = useState(0);
+    const [modalActive, setModalActive] = useState(false);
+    const [commentsModalActive, setCommentsModalActive] = useState(false);
+    const [comment, setComment] = useState('');
+    const [comments, setComments] = useState([]);
 
     const history = useHistory();
     const authCtx = useContext(AuthContext);
@@ -102,37 +112,120 @@ const Feed = (props) => {
         }
     };
 
+
+    const commentChangeHandler = (event) => {
+        setComment(event.target.value);
+    };
+
+    /* Handler for adding a comment */
+    const commentClickHandler = async () => {
+        setModalActive(true);
+    };
+
+    const getCommentsHandler = async (post) => {
+        const res = await fetch('http://localhost:5000/api/comments/get-comments?postId=' + post.id);
+        if (res.status === 200) {
+            const data = await res.json();
+            setComments(data.comments);
+            setCommentsModalActive(true);
+        } else {
+            /* do something */
+        }
+    };
+
+
+    const commentSubmitHandler = async (event, post) => {
+        event.preventDefault();
+        const res = await fetch('http://localhost:5000/api/comments/make-comment?postId=' + post.id, {
+            method: 'POST',
+            body: JSON.stringify({
+                creator: authCtx.userId,
+                content: comment
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (res.status === 200) {
+            setComment('');
+            setModalActive(false);
+            setNum(prev => prev + 1);
+        } else {
+            /* do something */
+        }
+    };
+
+
+    const xClickHandler = async () => {
+        setModalActive(false);
+        setCommentsModalActive(false);
+    };
+
     return (
-        <>
+        <div className={modalActive ? styles.hidden : ''}>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-            <Navigation />
-            {posts.length !== 0 &&
-                posts.reverse().map(post => {
-                    return (
-                        <div className={styles.post_container}>
-                            {post.userId === parseInt(authCtx.userId) && <Dots postId={post.id} onDelete={() => deleteHandler(post.id)} />}
-                            <h1 className={styles.post}>{post.title}</h1>
-                            <h4> By <strong className={styles.author} onClick={() => usernameClickHandler(post.userId)}>@{post.username}</strong></h4>
-                            <p className={styles.post}>{post.content}</p>
-                            <div className={styles.button_container}>
-                                <button className={styles.button} onClick={() => buttonClickHandler(post.id)}>Full Post</button>
+            <div className={modalActive || commentsModalActive ? styles.overlay : ''}>
+                <Navigation />
+                {posts.length !== 0 &&
+                    posts.reverse().map(post => {
+                        return (
+                            <div className={styles.post_container}>
+                                {post.userId === parseInt(authCtx.userId) && <Dots postId={post.id} onDelete={() => deleteHandler(post.id)} />}
+                                <h1 className={styles.post}>{post.title}</h1>
+                                <h4> By <strong className={styles.author} onClick={() => usernameClickHandler(post.userId)}>@{post.username}</strong></h4>
+                                <p className={styles.post}>{post.content}</p>
+                                <div className={styles.button_container}>
+                                    <button className={styles.button} onClick={() => buttonClickHandler(post.id)}>Full Post</button>
+                                </div>
+                                {!(likes.find(ele => ele.postId === post.id)) &&
+                                    <i className="fa fa-thumbs-up" onClick={() => thumbClickHandler(post, likes)}></i>
+                                }
+                                {likes.find(ele => ele.postId === post.id) &&
+                                    <>
+                                        <i className="fa fa-thumbs-up liked" onClick={() => thumbClickHandler(post, likes)}></i>
+                                    </>
+                                }
+                                <h5 className={styles.likes}>{post.numLikes} likes</h5>
+                                <div className={styles.comment_container}>
+                                    <i className="fa fa-comment" onClick={commentClickHandler} />
+                                    <h5 className={styles.comments} onClick={() => getCommentsHandler(post)}>{post.numComments} comments</h5>
+                                </div>
+                                {modalActive &&
+                                    <Modal className={styles.modal}>
+                                        <FontAwesomeIcon icon={faXmark} className={styles.x} onClick={xClickHandler} />
+                                        <div className={styles.modal_content}>
+                                            <form className={styles.comment_form} onSubmit={(event) => commentSubmitHandler(event, post)}>
+                                                <textarea className={styles.comment_area} placeholder='Write a meaningful comment...' value={comment} onChange={commentChangeHandler} />
+                                                <div className={styles.button_container}>
+                                                    <button className={styles.button} type='submit'>Submit</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </Modal>
+                                }
+                                {commentsModalActive && comments &&
+                                    <Modal className={styles.modal}>
+                                        <FontAwesomeIcon icon={faXmark} className={styles.x} onClick={xClickHandler} />
+                                        <div className={styles.modal_comment_content}>
+                                            {comments.map(comment => {
+                                                return (
+                                                    <div className={styles.list_comment}>
+                                                        <h3 className={styles.comment_username}>@{comment.username}</h3>
+                                                        <p className={styles.comment_content}>{comment.content}</p>
+                                                        <hr className={styles.hr}></hr>
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                    </Modal>
+                                }
                             </div>
-                            {!(likes.find(ele => ele.postId === post.id)) &&
-                                <i className="fa fa-thumbs-up" onClick={() => thumbClickHandler(post, likes)}></i>
-                            }
-                            {likes.find(ele => ele.postId === post.id) &&
-                                <>
-                                    <i className="fa fa-thumbs-up liked" onClick={() => thumbClickHandler(post, likes)}></i>
-                                </>
-                            }
-                            <h5 className={styles.likes}>{post.numLikes} likes</h5>
-                            <i className="fa fa-comment"></i>
-                            <h5 className={styles.comments}>{post.numComments} comments</h5>
-                        </div>)
-                }
-                )}
-            {posts.length === 0 && <div className={styles.post_container}><h1> No posts yet! </h1> </div>}
-        </>
+                        )
+                    }
+                    )}
+                {posts.length === 0 && <div className={styles.post_container}><h1> No posts yet! </h1> </div>}
+            </div>
+        </div>
     )
 };
 
--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -2,30 +2,45 @@
 const {validationResult} = require("express-validator");
 const Comment = require("../models/comment");
 const Post = require("../models/post");
+const User = require("../models/user");
 
 exports.postComment = async (req, res, next) => {
-    const errors = validationResult(req).array();
-    if (errors.length !== 0) {
-        return res
-            .status(404)
-            .json({
-                message: 'An error occurred'
-            });
-    }
+    // const errors = validationResult(req).array();
+    // if (errors.length !== 0) {
+    //     return res
+    //         .status(404)
+    //         .json({
+    //             message: 'An error occurred'
+    //         });
+    // }
     const content = req.body.content;
     const creator = req.body.creator;
-    const postId = req.params.postId;
+    const postId = req.query.postId;
     try {
+        const user = await User.findOne({
+            where: {
+                id: creator
+            }
+        });
         const newComment = await Comment.create({
             content: content,
             postId: postId,
-            userId: creator
+            userId: creator,
+            username: user.username
         });
+        const post = await Post.findOne({
+            where: {
+                id: postId
+            }
+        });
+        const currCount = post.numComments;
+        post.numComments = currCount + 1;
+        await post.save();
         res
             .status(200)
             .json({
                 message: 'Success',
-                comment: newComment.dataValues
+                comment: newComment
             });
     } catch (err) {
         res
@@ -67,7 +82,7 @@ exports.putComment = async (req, res, next) => {
     }
 };
 
-/* /api/comments/get-comments/:postId */
+/* /api/comments/get-comments?postId=7 */
 exports.getComments = async (req, res, next) => {
     const errors = validationResult(req).array();
     if (errors.length !== 0) {
@@ -77,14 +92,14 @@ exports.getComments = async (req, res, next) => {
                 message: 'An error occurred'
             });
     }
-    const postId = req.params.postId;
+    const postId = req.query.postId;
     try {
         const currPost = await Post.findByPk(postId);
         const currPostComments = await currPost.getComments();
         res
             .status(200)
             .json({
-                comment: currPostComments.dataValues
+                comments: currPostComments
             });
     } catch (err) {
         res
